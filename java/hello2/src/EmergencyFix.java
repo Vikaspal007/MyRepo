972dc334e1def82c81603088c79c9000697a2eee
vikas_pal <vikas@gmail.com>
2022-10-11 16:57:43 +0530

    second project updated

---
+++ java/hello2/src/EmergencyFix.java
@@ -0,0 +1,5 @@
+public class EmergencyFix {
+    public static void main(String[] args) {
+        System.out.println("Emergency fix");
+    }
+}
--- java/hello2/src/Java3.java
+++ java/hello2/src/Java3.java
@@ -1,5 +1,6 @@
 public class Java3 {
     public static void main(String[] args) {
         System.out.println("my second project");
+        System.out.println("update my second project");
     }
 }
